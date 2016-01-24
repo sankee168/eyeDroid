8e0374f68237bdd4603ba2bdec05236c7202f1b6
Venkat Sandeep <sandeep.kv92@gmail.com>
2016-01-23 19:15:53 -0500

    text to speech functions

---
+++ app/src/main/java/com/example/black/projectx/tts.java
@@ -0,0 +1,32 @@
+package com.example.black.projectx;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+import java.util.Locale;
+
+/**
+ * Created by sandy on 1/23/16.
+ */
+public class tts {
+    TextToSpeech ttobj;
+    Context c;
+
+    public tts(Context context) {
+        c = context;
+        ttobj = new TextToSpeech(c, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    ttobj.setLanguage(Locale.US);
+                }
+            }
+        });
+    }
+
+    public void speak(String s) {
+        //Toast.makeText(c, toSpeak,Toast.LENGTH_SHORT).show();
+        String utteranceId = this.hashCode() + "";
+        ttobj.speak(s, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+}
