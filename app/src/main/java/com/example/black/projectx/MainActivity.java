83946eff8a405a359a1265f8f1abf131c51f1ab0
Venkat Sandeep <sandeep.kv92@gmail.com>
2016-01-23 19:40:52 -0500

    testing tts

--- app/src/main/java/com/example/black/projectx/MainActivity.java
+++ app/src/main/java/com/example/black/projectx/MainActivity.java
@@ -4,14 +4,34 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ed1;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed1=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+
+        final tts t = new tts(this);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                //Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                //t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                t.speak(toSpeak);
+            }
+        });
 
     }
 
